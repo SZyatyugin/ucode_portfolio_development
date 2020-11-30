5f129aa00aba4eaedfaf1ff5862b88ca95da97d5
Stanislav Zyatyugin <zyatyugin.stas@gmail.com>
2020-11-30 09:43:55 +0200

    fixed position absolute of header elements

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,67 @@
-const path=require("path");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports={
-entry:"./src/index.js",
-output:{
-    path:path.resolve(__dirname,"dist"),
-    filename:"index.js",
-    publicPath:""
-},
-devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000,
-    overlay:true,
-    open:true,
-  },
-module:{
-    rules:[
-        {
-            test: /\.html$/i,
-            loader: 'html-loader',
-          },
-        {
-            test: /\.(sass|scss)$/i,
-            use: [
-                MiniCssExtractPlugin.loader,
-                "css-loader",
-                "sass-loader",
-                "postcss-loader",
-                'resolve-url-loader',  
-            ],  
-        },
-        {
-            test:/\.js$/,
-            exclude:/node_modules/,
-            use:["babel-loader","prettier-loader"],
-        },
-        {
-            test: /\.(png|jpe?g|gif)$/i,
-        loader: 'file-loader',
-        options: {
-          name: './assets/images/[name].[ext]',
-        },
-        },
-        {
-            test: /\.(svg|eot|woff|woff2|ttf)$/,
-                loader: "file-loader",
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+        publicPath: '',
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+        overlay: true,
+        open: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(sass|scss)$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                    'postcss-loader',
+                    'resolve-url-loader',
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'prettier-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: 'file-loader',
                 options: {
-                    name: "./assets/fonts/[name].[ext]",
+                    name: './assets/images/[name].[ext]',
                 },
-        }
-    ]
-},
-plugins: [
-    new MiniCssExtractPlugin({
-        filename:"style.css",
-    }),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-        filename:"index.html",
-        template:"./src/index.html"
-    })
-
-],
+            },
+            {
+                test: /\.(svg|eot|woff|woff2|ttf)$/,
+                loader: 'file-loader',
+                options: {
+                    name: './assets/fonts/[name].[ext]',
+                },
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html',
+        }),
+    ],
 }
